eb6e39c192c7132c6b5a9b3e8ee0cbe95588802d
kevinharr <kevinharr@verizon.net>
2023-02-28 14:39:30 -0500

    create Voter model

---
+++ models/vote.js
@@ -0,0 +1,50 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Vote extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Vote.init({
+    // value: DataTypes.INTEGER,
+    value: {
+      type: DataTypes.INTEGER,
+      validate: {
+        min: 0,
+				max: 5,
+      },
+    },
+
+    // profileId: DataTypes.INTEGER,
+    profileId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      },
+    },
+
+    // voterId: DataTypes.INTEGER,
+    voterId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      onDelete: 'CASCADE',
+      references: {
+        model: 'Profiles',
+        key: 'id',
+      },
+    },
+
+  })
+  return Vote;
+};
